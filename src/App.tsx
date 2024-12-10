5cf02a32a9556d9a04e9c156e3ff4c9f84ccc935
elkeipy <elkeipy81@gmail.com>
2024-12-10 18:22:43 +0900

    20241210

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,21 @@
-import React from 'react';
+import React, { Component, useState } from 'react';
 import logo from './asset/logo.svg';
 import './css/App.css';
+import { Button } from '@material-tailwind/react';
+import DraggableResizeModal from './components/DraggableResizeModal.jsx';
 
 function App() {
+  const [open, setOpen] = useState(false);
+  const [resizeOpen, setResizeOpen] = useState(false);
+
+  const handleOpenToggle = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setOpen(!open);
+  };
+
+  const handleOpenResizeToggle = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setResizeOpen(!resizeOpen);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
@@ -18,6 +31,31 @@ function App() {
         >
           Learn React
         </a>
+        <button color="blue" onClick={handleOpenToggle}>
+          Open Draggable Modal
+        </button>
+        <button color="red" onClick={handleOpenResizeToggle}>
+          Open Draggable(+resize) Modal
+        </button>
+        <DraggableResizeModal
+                title={'모달 테스트1'}
+                open={open}
+                width={450}
+                height={450}
+                onClose={handleOpenToggle}
+            >
+          test1
+        </DraggableResizeModal>
+        <DraggableResizeModal
+                title={'모달 테스트2'}
+                open={resizeOpen}
+                isResize={true}
+                width={450}
+                height={450}
+                onClose={handleOpenResizeToggle}
+            >
+          test2
+        </DraggableResizeModal>
       </header>
     </div>
   );
