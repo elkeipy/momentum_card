06aa5a2e7187aa6219e8d82552b01ec5cbe2b33c
elkeipy <elkeipy81@gmail.com>
2024-12-21 20:06:41 +0900

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@ import './css/index.css';
 import App from './App';
 //import reportWebVitals from './reportWebVitals';
 
-function TestApp() {
+function MinutesToHours() {
   const [counter, setCounter] = React.useState(0);
   const onClick = () => {
     //setCounter(counter + 1);
@@ -25,13 +25,14 @@ function TestApp() {
   
   return (
     <div>
-      <h1>Time Converter</h1>
       <label htmlFor='minutes'>Minutes</label>
-      <input value={flipped ? amount * 60 : amount} id='minutes' type="number" placeholder='Minutes' disabled={flipped}
+      <input value={flipped ? amount * 60 : amount} id='minutes' type="number" placeholder='Minutes' 
+        disabled={flipped}
         onChange={onChange}/>
       <h4>You want to convert {amount}</h4>
       <label htmlFor='hours'>Hours</label>
-      <input value={flipped ? amount : amount / 60} id='hours' type="number" placeholder='Hours' disabled={!flipped}
+      <input value={flipped ? amount : amount / 60} id='hours' type="number" placeholder='Hours' 
+        disabled={!flipped}
         onChange={onChange}/>
       <button onClick={() => {setAmount(0)}}>Reset</button>
       <button onClick={onFlip}>Flip</button>
@@ -39,6 +40,52 @@ function TestApp() {
         Total Click: {counter}
       </h3>
       <button onClick={onClick}>Click Click</button>
+    </div>
+  )
+}
+
+function KmToMiles() {
+  const [distance, setDistance ] = React.useState(0);
+  const [flipped, setFlipped] = React.useState(true);
+
+  return (
+    <div>
+      <h3>Km to Miles</h3>
+      <label>Km</label>
+      <input disabled={!flipped} 
+        placeholder='Km' type='number'
+        value={!flipped ? distance * 0.62 : distance}
+        onChange={(event: ChangeEvent<HTMLInputElement>) => setDistance(parseInt(event.target.value))}>
+      </input>
+      <hr/>
+      <label>Miles</label> 
+      <input disabled={flipped}
+        placeholder='Mile' type='number'
+        value={flipped ? distance * 1.61 : distance}
+        onChange={(event: ChangeEvent<HTMLInputElement>) => setDistance(parseInt(event.target.value))}>
+      </input>
+      <button onClick={() => setDistance(0)}>Reset</button>
+      <button onClick={() => setFlipped(!flipped)}>flip</button>
+    </div>
+  )
+}
+
+function TestApp() {
+  const [index, setIndex] = React.useState<undefined | string>();
+  const onSelect = (event: ChangeEvent<HTMLSelectElement>) => {
+    console.log(event);
+    setIndex(event.target.value);
+  }
+  return (
+    <div>
+      <h1>Multi Converter</h1>
+      <select value={index} onChange={onSelect}>
+        <option value="0">Minutes & Hours</option>
+        <option value="1">Km & Miles</option>
+      </select>
+      <hr/>
+      {index === "0" ? <MinutesToHours/> : null}
+      {index === "1" ? <KmToMiles/> : null}
     </div>
   )
 }
